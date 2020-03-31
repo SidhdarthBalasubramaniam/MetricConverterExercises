41318405b584f6bca497dcf5c0b27d2299db77c5
Sidhdarth <sidhdarthsidhu@gmail.com>
2020-03-31 11:44:53 +0200

    uncommented the lines in main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
  #include "tests/tests_task2.h"
  #include "tests/tests_task3.h"
-// #include "tests/tests_task4.h"
+#include "tests/tests_task4.h"
 
 int main() {
   /* Task 2 */
@@ -17,10 +17,10 @@ int main() {
 
   /* Task 4 */
 
-  // test_metres_to_millimetres();
-  // test_millimetres_to_metres();
-  // test_kilometres_to_millimetres();
-  // test_millimetres_to_kilometres();
-  // test_kilometres_to_centimetres();
-  // test_centimetres_to_kilometres();
+  test_metres_to_millimetres();
+  test_millimetres_to_metres();
+  test_kilometres_to_millimetres();
+  test_millimetres_to_kilometres();
+  test_kilometres_to_centimetres();
+  test_centimetres_to_kilometres();
 }
